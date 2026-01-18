f33f9f8626b293a6a5f3e60ae6153c41aca7836c
Your Name <you@example.com>
2026-01-18 15:28:27 +0000

    'auto'

--- app/src/server/express-server.js
+++ app/src/server/express-server.js
@@ -69,12 +69,7 @@ class ExpressServer {
       this.app.get('/screen', this.handlePyAutoGUIScreenshot.bind(this));
 
       // PyAutoGUI screenshot with path endpoint
-      this.app.get('/screenpath/:filename', this.handlePyAutoGUIScreenshotPath.bind(this));
-
-      // Additional screenshot endpoints
-      this.app.get('/screen/path/:filename', this.handlePyAutoGUIScreenshotPath.bind(this));
       this.app.get('/screen/pth/:filename', this.handlePyAutoGUIScreenshotPath.bind(this));
-      this.app.get('/screepath/:filename?', this.handlePyAutoGUIScreenshotPath.bind(this));
 
     // Legacy RPC endpoint
     this.app.post('/rpc', this.handleRpc.bind(this));
